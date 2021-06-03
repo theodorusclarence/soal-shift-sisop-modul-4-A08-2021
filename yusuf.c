0db437e1cfeb35e72d4448eef8613265c113723a
YusufAnfasya <yprikitiw71@gmail.com>
2021-06-02 21:49:25 -0700

    Soal 4 dicek dulu ya,kurang pandai soalnya

---
+++ yusuf.c
@@ -0,0 +1,50 @@
+void loglvlWarning(const char *log, const char *path){
+    FILE *fp;
+    fp = fopen("/home/yusuf/SinSeiFS.log", "a");
+    fputs("WARNING::", fp);
+    char timestamp[1000];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(timestamp, "%02d%02d%02d-%02d:%02d:%02d:", (tm.tm_year + 1900)%100, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    fputs(timestamp, fp);
+    fputs(log, fp);
+    fputs("::", fp);
+    fputs(path, fp);
+    fputs("\n", fp);
+    fclose(fp);
+}
+
+void loglvlInfo(const char *log, const char *path){
+    FILE *fp;
+    fp = fopen("/home/yusuf/SinSeiFS.log", "a");
+    fputs("INFO::", fp);
+    char timestamp[1000];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(timestamp, "%02d%02d%02d-%02d:%02d:%02d:", (tm.tm_year + 1900)%100, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    fputs(timestamp, fp);
+    fputs(log, fp);
+    fputs("::", fp);
+    fputs(path, fp);
+    fputs("\n", fp);
+    fclose(fp);
+}
+
+// Membuat log dengan pesan Info tapi menerima dua path, source dan destination
+void loglvlInfo2(const char *log, const char *source, const char *destination){
+    FILE *fp;
+    fp = fopen("/home/yusuf/SinSeiFS.log", "a");
+    fputs("INFO::", fp);
+    char timestamp[1000];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(timestamp, "%02d%02d%02d-%02d:%02d:%02d:", (tm.tm_year + 1900)%100, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    fputs(timestamp, fp);
+    fputs(log, fp);
+    fputs("::", fp);
+    fputs(source, fp);
+    fputs("::", fp);
+    fputs(destination, fp);
+    fputs("\n", fp);
+    fclose(fp);
+}
