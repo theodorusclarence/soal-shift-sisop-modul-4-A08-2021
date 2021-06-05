42f02c1f6a836cdaf6d1190de03d18c18284bb54
MuhammadNevin <nevin.muhammad@gmail.com>
2021-06-05 15:36:40 +0700

    Nevin

--- nevin.c
+++ nevin.c
@@ -51,6 +51,193 @@ void atbash(char str[1000], char newStr[1000]) {
   newStr[i] = '\0';
 }
 
+void rot13Enc(char str[1000], char newStr[1000]) {
+  // Skip if directory
+  if (!strcmp(str, ".") || !strcmp(str, "..")) {
+    strcpy(newStr, str);
+    return;
+  };
+
+  int i, flag = 0;
+  i = 0;
+  while (str[i] != '\0') {
+    // exclude extension
+    if (str[i] == '.') {
+      flag = 1;
+    }
+    if (flag == 1) {
+      newStr[i] = str[i];
+      i++;
+      continue;
+    }
+
+    if (!((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97))) {
+      if (str[i] >= 'A' && str[i] <= 'Z') {
+        if(str[i] + 13 > 'Z') {
+          newStr[i] = 'A' + (12 - ('Z' - str[i]));
+        }
+        else {
+          newStr[i] = str[i];
+        }
+      }
+      if (str[i] >= 'a' && str[i] <= 'z') {
+        if(str[i] + 13 > 'z') {
+          newStr[i] = 'a' + (12 - ('z' - str[i]));
+        }
+        else {
+          newStr[i] = str[i];
+        }
+      }
+    }
+
+    if ((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97)) {
+      newStr[i] = str[i];
+    }
+
+    i++;
+  }
+  newStr[i] = '\0';
+}
+
+void rot13Denc(char str[1000], char newStr[1000]) {
+  // Skip if directory
+  if (!strcmp(str, ".") || !strcmp(str, "..")) {
+    strcpy(newStr, str);
+    return;
+  };
+
+  int i, flag = 0;
+  i = 0;
+  while (str[i] != '\0') {
+    // exclude extension
+    if (str[i] == '.') {
+      flag = 1;
+    }
+    if (flag == 1) {
+      newStr[i] = str[i];
+      i++;
+      continue;
+    }
+
+    if (!((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97))) {
+      if (str[i] >= 'A' && str[i] <= 'Z') {
+        if(str[i] - 13 < 'A') {
+          newStr[i] = 'Z' - (12 - (str[i] - 'A'));
+        }
+        else {
+          newStr[i] = str[i];
+        }
+      }
+      if (str[i] >= 'a' && str[i] <= 'z') {
+        if(str[i] - 13 < 'a') {
+          newStr[i] = 'z' - (12 - (str[i] - 'a'));
+        }
+        else {
+          newStr[i] = str[i];
+        }
+      }
+    }
+
+    if ((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97)) {
+      newStr[i] = str[i];
+    }
+
+    i++;
+  }
+  newStr[i] = '\0';
+}
+
+void vigen(char str[1000], char newStr[1000], bool encr) {
+  // Skip if directory
+  if (!strcmp(str, ".") || !strcmp(str, "..")) {
+    strcpy(newStr, str);
+    return;
+  };
+
+  char key[] = "SISOP";
+  int msgLen = strlen(str), keyLen = strlen(key), i, j;
+  char newKey[msgLen];
+
+  //generating new key
+  for(i = 0,j = 0; i < msgLen; ++i, ++j){
+      if(j == keyLen)
+          j = 0;
+
+      newKey[i] = key[j];
+  }
+  newKey[i] = '\0';
+
+  int flag = 0;
+  i = 0;
+  while (str[i] != '\0') {
+    char temp;
+
+    // exclude extension
+    if (str[i] == '.') {
+      flag = 1;
+    }
+    if (flag == 1) {
+      newStr[i] = str[i];
+      i++;
+      continue;
+    }
+
+    if(encr) {
+      if (!((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97))) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+          if(newKey[i] >= 'a' && newKey[i] <= 'z') {
+              temp = newKey[i] - 'a' + 'A';
+              newStr[i] = ((str[i] + temp) % 26) + 'A';
+          }
+          else {
+              newStr[i] = ((str[i] + newKey[i]) % 26) + 'A';
+          }
+        }
+        if (str[i] >= 'a' && str[i] <= 'z') {
+          temp = str[i] - 'a' + 'A';
+          if(newKey[i] >= 'a' && newKey[i] <= 'z') {
+              char tempNK = newKey[i] - 'a' + 'A';
+              newStr[i] = ((temp + tempNK) % 26) + 'A';
+          }
+          else {
+              newStr[i] = ((temp + newKey[i]) % 26) + 'A';
+          }
+          newStr[i] = newStr[i] - 'A' + 'a';
+        }
+      }
+    }
+    else {
+      if ((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97)) {
+        if (!((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97))) {
+          if (str[i] >= 'A' && str[i] <= 'Z') {
+            if(newKey[i] >= 'a' && newKey[i] <= 'z') {
+                temp = newKey[i] - 'a' + 'A';
+                newStr[i] = (((str[i] + temp) + 26) % 26) + 'A';
+            }
+            else {
+                newStr[i] = (((str[i] + newKey[i]) + 26) % 26) + 'A';
+            }
+          }
+          if (str[i] >= 'a' && str[i] <= 'z') {
+            temp = str[i] - 'a' + 'A';
+            if(newKey[i] >= 'a' && newKey[i] <= 'z') {
+                char tempNK = newKey[i] - 'a' + 'A';
+                newStr[i] = (((temp + tempNK) + 26) % 26) + 'A';
+            }
+            else {
+                newStr[i] = (((temp + newKey[i]) + 26) % 26) + 'A';
+            }
+            newStr[i] = newStr[i] - 'A' + 'a';
+          }
+        }
+      }
+    }
+
+    i++;
+  }
+  newStr[i] = '\0';
+}
+
 static int xmp_getattr(const char *path, struct stat *stbuf) {
   int res;
   char fpath[1000];
@@ -61,7 +248,7 @@ static int xmp_getattr(const char *path, struct stat *stbuf) {
 
   sprintf(fpath, "%s%s", dirpath, path);
 
-  bool hasToEncrypt = false;
+  int hasToEncrypt = 0;
 
   // ? Current Path -> directory sekarang
   // ? pathEnc -> path yang harus didecrypt biar bisa dibaca sama si attr
@@ -76,10 +263,13 @@ static int xmp_getattr(const char *path, struct stat *stbuf) {
       strcat(currPath, "/");
       strcat(currPath, token);
     }
-    
+
     // TODO Check if prefixed with atoz_
     if (strncmp(token, "AtoZ_", 5) == 0) {
-      hasToEncrypt = true;
+      hasToEncrypt = 1;
+    }
+    else if (strncmp(token, "RX_[Nama]", 9) == 0) {
+      hasToEncrypt = 2;
     }
     token = strtok(NULL, "/");
   }
@@ -104,7 +294,7 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
   char fpath[1000];
   printf("\n🚀 READDIR\n");
 
-  bool hasToEncrypt = false;
+  int hasToEncrypt = 0;
   char currPath[1000] = "", pathEnc[1000] = "";
 
   char *token = strtok(path, "/");
@@ -119,7 +309,7 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
     // TODO Check if prefixed with atoz_
     if (strncmp(token, "AtoZ_", 5) == 0) {
-      hasToEncrypt = true;
+      hasToEncrypt = 1;
     }
     printf("token: %s\n", token);
     token = strtok(NULL, "/");
@@ -178,7 +368,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 
   sprintf(fpath, "%s%s", dirpath, path);
 
-  bool hasToEncrypt = false;
+  int hasToEncrypt = 0;
 
   // ? Current Path -> directory sekarang
   // ? pathEnc -> path yang harus didecrypt biar bisa dibaca sama si attr
@@ -196,8 +386,9 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 
     // TODO Check if prefixed with atoz_
     if (strncmp(token, "AtoZ_", 5) == 0) {
-      hasToEncrypt = true;
+      hasToEncrypt = 1;
     }
+    else if(strcmp(token, ""))
     token = strtok(NULL, "/");
   }
 
@@ -228,10 +419,75 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
   return res;
 }
 
+static int xmp_rename(const char *from, const char *to) {
+  char fpathFrom[1000];
+  char fpathTo[1000];
+
+  if (strcmp(from, "/") == 0) {
+    from = dirpath;
+    sprintf(fpathFrom, "%s", from);
+  } else {
+    sprintf(fpathFrom, "%s%s", dirpath, from);
+  }
+
+  if (strcmp(to, "/") == 0) {
+    to = dirpath;
+    sprintf(fpathTo, "%s", to);
+  } else {
+    sprintf(fpathTo, "%s%s", dirpath, to);
+  }
+
+  int res;
+
+  res = rename(fpathFrom, fpathTo);
+  if (res == -1) return -errno;
+
+  return 0;
+}
+
+static int xmp_mkdir(const char *path, mode_t mode) {
+  char fpath[1000];
+  if (strcmp(path, "/") == 0) {
+    path = dirpath;
+    sprintf(fpath, "%s", path);
+  } else {
+    sprintf(fpath, "%s%s", dirpath, path);
+  }
+  int res;
+  res = mkdir(fpath, mode);
+
+  if (res == -1) return -errno;
+
+  return 0;
+}
+
+static int xmp_create(const char *path, mode_t mode,
+                      struct fuse_file_info *fi) {
+  char fpath[1000];
+  if (strcmp(path, "/") == 0) {
+    path = dirpath;
+    sprintf(fpath, "%s", path);
+  } else {
+    sprintf(fpath, "%s%s", dirpath, path);
+  }
+
+  (void)fi;
+
+  int res;
+  res = creat(fpath, mode);
+  if (res == -1) return -errno;
+
+  close(res);
+  return 0;
+}
+
 static struct fuse_operations xmp_oper = {
     .getattr = xmp_getattr,
     .readdir = xmp_readdir,
     .read = xmp_read,
+    .rename = xmp_rename,
+    .mkdir = xmp_mkdir,
+    .create = xmp_create,
 };
 
 int main(int argc, char *argv[]) {
